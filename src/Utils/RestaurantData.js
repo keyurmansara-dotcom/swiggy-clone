f5c1f8753cdbe3edebf98192f888358fdbd13dc8
keyurmansara-dotcom <keyurmansara@gmail.com>
2025-12-27 10:56:12 +0530

    Initial commit

---
+++ src/Utils/RestaurantData.js
@@ -0,0 +1,55 @@
+export const mockRestaurants = {
+    "data": {
+        "cards": [
+            {},
+            {
+                "card": {
+                    "card": {
+                        "gridElements": {
+                            "infoWithStyle": {
+                                "restaurants": [
+                                    {
+                                        "info": {
+                                            "id": "1",
+                                            "name": "Sample Restaurant 1",
+                                            "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+                                            "locality": "Local Area",
+                                            "areaName": "Sample Area",
+                                            "costForTwo": "₹200 for two",
+                                            "cuisines": ["North Indian", "Chinese"],
+                                            "avgRating": 4.2,
+                                            "feeDetails": {
+                                                "totalFee": 30
+                                            },
+                                            "sla": {
+                                                "deliveryTime": 25
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "info": {
+                                            "id": "2",
+                                            "name": "Sample Restaurant 2",
+                                            "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+                                            "locality": "Local Area 2",
+                                            "areaName": "Sample Area 2",
+                                            "costForTwo": "₹300 for two",
+                                            "cuisines": ["South Indian", "Chinese"],
+                                            "avgRating": 4.5,
+                                            "feeDetails": {
+                                                "totalFee": 40
+                                            },
+                                            "sla": {
+                                                "deliveryTime": 30
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            }
+        ]
+    }
+}
